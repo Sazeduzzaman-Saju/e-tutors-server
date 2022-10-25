78a3f5a07fb7f80ad5b92462fcf36a9f3db3496a
Sazeduzzaman <sazeduzzaman@yahoo.com>
2022-10-25 07:53:43 +0600

    etutor server finished

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const Port = process.env.Port || 5000;
 
 app.use(cors());
 
+// import/get data json file
 const courseTitle = require('./data/CoursesTitle.json');
 const courseDetails = require('./data/allCourses.json')
 
@@ -14,19 +15,28 @@ app.get('/', (req, res) => {
     res.send('Courses Api Running ')
 })
 
-
+// all course title
 app.get('/course-title', (req, res) => {
     res.send(courseTitle);
 })
+
+// single course title
+app.get('/course-title/:id', (req, res) => {
+    const id = req.params.id;
+    const singleCourseTitle = courseTitle.find(c => c.id == id)
+    res.send(singleCourseTitle)
+})
+
+// all course Details 
 app.get('/course-details', (req, res) => {
     res.send(courseDetails);
 })
 
-app.get('/course-title/:id', (req, res) => {
+// single course Details with id
+app.get('/course-details/:id', (req, res) => {
     const id = req.params.id;
-    const singleCourse = courseTitle.find(n => n.id === id);
-    res.send(singleCourse)
-    console.log(id, singleCourse)
+    const singleCourse = courseDetails.filter(courses => courses.id == id)
+    res.send(singleCourse);
 })
 
 // running api server
